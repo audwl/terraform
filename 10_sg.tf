e8a48b39429ea221f10db35f805ed9a24a97e108
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 22:05:47 +0900

    Update 10_sg.tf

--- 10_sg.tf
+++ 10_sg.tf
@@ -1,59 +1,68 @@
 resource "aws_security_group" "mjkim_websg" {
     name = "Allow-WEB"
     description = "http-ssh-icmp"
-    vpc_id = aws_vpc.mjkim-vpc.id
-    ingress = [
-        {
-        description = "ssh"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        security_groups = null
-        prefix_list_ids = null
-        self = null
-},
-
- {
-        description = "http"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        security_groups = null
-        prefix_list_ids = null
-        self = null
-},
-
- {
-        description = "icmp"
-        from_port = -1
-        to_port = -1
-        protocol = "icmp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        security_groups = null
-        prefix_list_ids = null
-        self = null
-}
-]
-
-egress = [
+    vpc_id = aws_vpc.mjkim_vpc.id
+     ingress = [
     {
-        description = "All"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        security_groups = null
-        prefix_list_ids = null
-        self = null
-    }]
-tags = {
-  Name = "mjkim-sg"
-}
-  
-}
+      description       = "ssh"
+      from_port         = 22
+      to_port           = 22
+      protocol          = "tcp"
+      cidr_blocks       = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      security_groups   =  null
+      prefix_list_ids   =  null
+      self              =  null
+    },
+    {
+      description     = "http"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      security_groups  =  null
+      prefix_list_ids  =  null
+      self             =  null
+    },
+    {
+      description     = "icmp"
+      from_port       = -1
+      to_port         = -1
+      protocol        = "icmp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      security_groups  =  null
+      prefix_list_ids  =  null
+      self             =  null
+    },
+    {
+      description     = "mysql"
+      from_port       = 3306
+      to_port         = 3306
+      protocol        = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = ["::/0"]
+      security_groups  =  null
+      prefix_list_ids  =  null
+      self             =  null
+    }
+  ]
+
+   egress = [
+     {
+      description     = "All"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      security_groups  =  null
+      prefix_list_ids  =  null
+      self             =  null
+    }
+   ]
+  tags = {
+    Name = "mjkim-sg"
+  }
+} 
