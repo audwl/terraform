34dae622fcf2d1eb1dbef83a5d7906b9c10276c4
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 22:08:13 +0900

    Update 12_alb.tf

--- 12_alb.tf
+++ 12_alb.tf
@@ -3,10 +3,10 @@ resource "aws_lb" "mjkim_lb" {
     internal = false
     load_balancer_type = "application"
     security_groups = [aws_security_group.mjkim_websg.id]
-    subnets = [aws_subnet.mjkim-puba.id,aws_subnet.mjkim-pubc.id]
+    subnets = [aws_subnet.mjkim_puba.id,aws_subnet.mjkim_pubc.id]
     
     tags= {
-      Name = "mjkim=alb"
+      Name = "mjkim-alb"
     }
 
   
