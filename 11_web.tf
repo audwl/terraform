1215593bee53bb2d989cae14a934e909580129ac
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 22:06:49 +0900

    Update 11_web.tf

--- 11_web.tf
+++ 11_web.tf
@@ -14,14 +14,14 @@ data "aws_ami" "amzn" {
   owners = ["amazon"]
 }
 
-resource "aws_instance" "mjkim_weba" {
+resource "aws_instance" "sdkim_weba" {
   ami                    = data.aws_ami.amzn.id
   instance_type          = "t2.micro"
   key_name               = "tf-key"
   vpc_security_group_ids = [aws_security_group.mjkim_websg.id]
   availability_zone      = "ap-northeast-2a"
   private_ip             = "10.0.0.11"
-  subnet_id              = aws_subnet.mjkim-puba.id
+  subnet_id              = aws_subnet.mjkim_puba.id
   user_data              =  file("./install_seoul.sh") 
 
   tags = {
@@ -33,5 +33,5 @@ resource "aws_eip" "mjkim_web_eip" {
   vpc = true
   instance                    = aws_instance.mjkim_weba.id
   associate_with_private_ip   = "10.0.0.11"
-  depends_on                  = [aws_internet_gateway.mjkim-ig]
+  depends_on                  = [aws_internet_gateway.mjkim_ig]
 }
