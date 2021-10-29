fae7e799b5dd7ed920d7aa65e0ce016f7c42fc66
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 14:25:11 +0900

    Update 11_web.tf

--- 11_web.tf
+++ 11_web.tf
@@ -1,30 +1,37 @@
 data "aws_ami" "amzn" {
-    most_recent = true
-    
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-ebs"]
-    }
-    filter {
-      name = "virtualization-type"
-      values = ["hvm"]
-    }
+  most_recent = true
 
-    owners = ["amazon"]
-  
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
 }
-resource "aws_instance" "mjkim_weba" {
-    ami = data.aws_ami.amzn.id
-    instance_type = "t2.micro"
-    key_name = "tf-key"
-    vpc_security_group_ids = [aws_security_group.mjkim_websg.id]
-    availability_zone = "ap-northeast-2a" 
-    private_ip = "10.0.0.11"
-    subnet_id = aws_subnet.mjkim-puba.id
-    user_data = file("../../Desktop/install_seoul.sh")
 
-    tags = {
-      name = "mjkim-weba"
-    }
-  
+resource "aws_instance" "mjkim_weba" {
+  ami                    = data.aws_ami.amzn.id
+  instance_type          = "t2.micro"
+  key_name               = "tf-key"
+  vpc_security_group_ids = [aws_security_group.mjkim_websg.id]
+  availability_zone      = "ap-northeast-2a"
+  private_ip             = "10.0.0.11"
+  subnet_id              = aws_subnet.mjkim_puba.id
+  user_data              =  file("./install_seoul.sh") 
+
+  tags = {
+    Name = "mjkim-weba"
+  }
+}
+
+resource "aws_eip" "mjkim_web_eip" {
+  vpc = true
+  instance                    = aws_instance.mjkim_weba.id
+  associate_with_private_ip   = "10.0.0.11"
+  depends_on                  = [aws_internet_gateway.mjkim_ig]
 }
