3fdaea2e10d416ae8c158d1d39990f45b6da5eea
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 22:13:57 +0900

    Update 18_autoscgr.tf

--- 18_autoscgr.tf
+++ 18_autoscgr.tf
@@ -2,8 +2,8 @@ resource "aws_placement_group" "mjkim_pg" {
     name = "mjkim-pg"
     strategy = "cluster"
 }
-resource "aws_autoscaling_group" "mjkim_at_sg" {
-    name = "mjkim-at-sg"
+resource "aws_autoscaling_group" "mjkim_atsg" {
+    name = "mjkim-atsg"
     min_size = 2
     max_size = 8
     health_check_grace_period = 300
@@ -11,5 +11,5 @@ resource "aws_autoscaling_group" "mjkim_at_sg" {
     desired_capacity = 2
     force_delete = true
     launch_configuration = aws_launch_configuration.mjkim_lacf.name
-    vpc_zone_identifier = [aws_subnet.mjkim-puba.id,aws_subnet.mjkim-pubc.id]
+    vpc_zone_identifier = [aws_subnet.mjkim_puba.id,aws_subnet.mjkim_pubc.id]
 }
