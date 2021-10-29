ecd68babe0f5950946f5988f320990f751ea16f0
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 22:00:38 +0900

    Update 06_rtas.tf

--- 06_rtas.tf
+++ 06_rtas.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table_association" "mjkim_rtas_a" {
-    subnet_id = aws_subnet.mjkim-puba.id
+    subnet_id = aws_subnet.mjkim_puba.id
     route_table_id = aws_route_table.mjkim_rt.id
 }
 resource "aws_route_table_association" "mjkim_rtas_c" {
-    subnet_id = aws_subnet.mjkim-pubc.id
+    subnet_id = aws_subnet.mjkim_pubc.id
     route_table_id = aws_route_table.mjkim_rt.id
 }
