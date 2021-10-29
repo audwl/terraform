5f9b1678b536f82a9e57f1e18f5c30c8b0ffc3a6
audwl <93104498+audwl@users.noreply.github.com>
2021-10-29 21:58:41 +0900

    Update 03_sub.tf

--- 03_sub.tf
+++ 03_sub.tf
@@ -1,6 +1,6 @@
 # 가용영역 a의 Public Subnet
-resource "aws_subnet" "mjkim-puba" {
-    vpc_id = aws_vpc.mjkim-vpc.id
+resource "aws_subnet" "mjkim_puba" {
+    vpc_id = aws_vpc.mjkim_vpc.id
     cidr_block = "10.0.0.0/24"
     availability_zone = "ap-northeast-2a"
     tags = {
@@ -9,8 +9,8 @@ resource "aws_subnet" "mjkim-puba" {
   
 }
 #가용영역 a의 Private Subnet
-resource "aws_subnet" "mjkim-pria" {
-    vpc_id = aws_vpc.mjkim-vpc.id
+resource "aws_subnet" "mjkim_pria" {
+    vpc_id = aws_vpc.mjkim_vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "ap-northeast-2a"
     tags = {
@@ -19,8 +19,8 @@ resource "aws_subnet" "mjkim-pria" {
   
 }
 # 가용영역 c의 Public Subnet
-resource "aws_subnet" "mjkim-pubc" {
-    vpc_id = aws_vpc.mjkim-vpc.id
+resource "aws_subnet" "mjkim_pubc" {
+    vpc_id = aws_vpc.mjkim_vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "ap-northeast-2c"
     tags = {
@@ -29,8 +29,8 @@ resource "aws_subnet" "mjkim-pubc" {
   
 }
 #가용영역 c의 Private Subnet
-resource "aws_subnet" "mjkim-pric" {
-    vpc_id = aws_vpc.mjkim-vpc.id
+resource "aws_subnet" "mjkim_pric" {
+    vpc_id = aws_vpc.mjkim_vpc.id
     cidr_block = "10.0.3.0/24"
     availability_zone = "ap-northeast-2c"
     tags = {
