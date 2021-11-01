84d52e7a3c76c0ebbbc430a318e21f3932df3704
audwl <93104498+audwl@users.noreply.github.com>
2021-11-01 10:12:28 +0900

    Update 11_web.tf

--- 11_web.tf
+++ 11_web.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amzn" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
@@ -14,21 +14,20 @@ data "aws_ami" "amzn" {
   owners = ["amazon"]
 }
 
-resource "aws_instance" "sdkim_weba" {
-  ami                    = data.aws_ami.amzn.id
+resource "aws_instance" "mjkim_weba" {
+  ami                    = "ami-0e4a9ad2eb120e054"                             #data.aws_ami.amzn.id
   instance_type          = "t2.micro"
-  key_name               = "tf-key"
+  key_name               = "tf-key1"
   vpc_security_group_ids = [aws_security_group.mjkim_websg.id]
   availability_zone      = "ap-northeast-2a"
   private_ip             = "10.0.0.11"
   subnet_id              = aws_subnet.mjkim_puba.id
-  user_data              =  file("./install_seoul.sh") 
+  user_data              = file("./install_seoul.sh") 
 
   tags = {
     Name = "mjkim-weba"
   }
 }
-
 resource "aws_eip" "mjkim_web_eip" {
   vpc = true
   instance                    = aws_instance.mjkim_weba.id
