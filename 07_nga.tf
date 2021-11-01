a0bf4c3a2e50d116c9b5d6145ec3e816b7704d7e
audwl <93104498+audwl@users.noreply.github.com>
2021-11-01 10:11:56 +0900

    Update 07_nga.tf

--- 07_nga.tf
+++ 07_nga.tf
@@ -9,7 +9,7 @@ resource "aws_eip" "lb_ip_c" {
 
 resource "aws_nat_gateway" "mjkim_nga_a" {
     allocation_id = aws_eip.lb_ip_a.id
-    subnet_id = aws_subnet.mjkim_pria.id
+    subnet_id = aws_subnet.mjkim_puba.id
     tags = {
       Name = "mjkim-nga-a"
     }
@@ -17,7 +17,7 @@ resource "aws_nat_gateway" "mjkim_nga_a" {
 }
 resource "aws_nat_gateway" "mjkim_nga_c" {
     allocation_id = aws_eip.lb_ip_c.id
-    subnet_id = aws_subnet.mjkim_pric.id
+    subnet_id = aws_subnet.mjkim_pubc.id
     tags = {
       Name = "mjkim-nga-c"
     }
